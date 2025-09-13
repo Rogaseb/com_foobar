33b56cdd59115edc3b17c1c7e3f2a3c54edc3cbf
SebUbu <rogaseb@gmail.com>
2025-09-13 10:16:03 +0200

    Zaktualizowano szablon edycji formularza w pliku edit.php dla widoku foo

--- admin/tmpl/foo/edit.php
+++ admin/tmpl/foo/edit.php
@@ -18,6 +18,8 @@ $wa->useScript('keepalive')
     <?php echo LayoutHelper::render('joomla.edit.title_alias', $this); ?>
 
     <div class="main-card">
+        <?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => 'details', 'recall' => true, 'breakpoint' => 768]); ?>
+
         <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'publishing', Text::_('JGLOBAL_FIELDSET_PUBLISHING')); ?>
         <div class="row">
             <div class="col-12 col-lg-6">
@@ -30,7 +32,10 @@ $wa->useScript('keepalive')
             </div>
         </div>
         <?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+        <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
     </div>
 
+    <input type="hidden" name="task" value="">
     <?php echo HTMLHelper::_('form.token'); ?>
 </form>
