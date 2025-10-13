bba765d977a260237cd0578be7180f466774c570
SebUbu <rogaseb@gmail.com>
2025-10-13 13:59:08 +0200

    Dodanie wyszukiwania i filtrowania

--- admin/src/Model/FoosModel.php
+++ admin/src/Model/FoosModel.php
@@ -5,6 +5,7 @@ namespace FooSpace\Component\Foobar\Administrator\Model;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\Model\ListModel;
+use Joomla\Database\ParameterType;
 
 class FoosModel extends ListModel
 {
@@ -13,18 +14,48 @@ class FoosModel extends ListModel
         $db    = $this->getDatabase();
         $query = $db->getQuery(true);
 
-        $query->select('*')
+        $query->select([
+                $db->quoteName('a.id'),
+                $db->quoteName('a.title'),
+                $db->quoteName('a.alias'),
+                $db->quoteName('a.state'),
+                $db->quoteName('a.ordering'),
+            ])
             ->from($db->quoteName('#__foobar_foos', 'a'));
+
+        $published = (string) $this->getState('filter.published');
+        if ($published !== '') {
+            $pubInt = (int) $published;
+            $query->where($db->quoteName('a.state') . ' = :pub')
+                ->bind(':pub', $pubInt, ParameterType::INTEGER);
+        } else {
+            $query->where($db->quoteName('a.state') . ' IN (0,1)');
+        }
+
+        $search = (string) $this->getState('filter.search');
+        if ($search !== '') {
+            if (stripos($search, 'id:') === 0) {
+                $id = (int) substr($search, 3);
+                $query->where($db->quoteName('a.id') . ' = :id')
+                    ->bind(':id', $id, ParameterType::INTEGER);
+            } else {
+                $token = '%' . str_replace(' ', '%', trim($search)) . '%';
+                $query->where(
+                    '(' . $db->quoteName('a.title') . ' LIKE :tok1'
+                    . ' OR ' . $db->quoteName('a.alias') . ' LIKE :tok2)'
+                );
+                $query->bind([':tok1', ':tok2'], $token, ParameterType::STRING);
+            }
+        }
 
         return $query;
     }
 
-    public function getTable($name = 'Foo', $prefix = 'Table', $options = array())
+    protected function getStoreId($id = '')
     {
-        if ($table = $this->_createTable($name, $prefix, $options))
-        {
-            return $table;
-        }
-        return null;
+        $id .= ':' . $this->getState('filter.search');
+        $id .= ':' . $this->getState('filter.published');
+
+        return parent::getStoreId($id);
     }
 }
--- admin/tmpl/foos/default.php
+++ admin/tmpl/foos/default.php
@@ -2,18 +2,17 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
 ?>
 
-<?php if (empty($this->items)) : ?>
-    <?php echo $this->loadTemplate('emptystate'); ?>
-<?php else : ?>
-
 <form action="<?php echo Route::_('index.php?option=com_foobar&view=foos'); ?>" method="post" name="adminForm" id="adminForm">
-	<div id="j-main-container" class="j-main-container">
+    <div id="j-main-container" class="j-main-container">
+
+        <?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
 
         <?php if (empty($this->items)) : ?>
             <div class="alert alert-info">
@@ -22,46 +21,44 @@ use Joomla\CMS\Router\Route;
             </div>
         <?php else : ?>
 
-		<table class="table table-striped table-hover">
-			<thead>
-				<tr>
-					<td class="w-1 text-center">
-						<?php echo HTMLHelper::_('grid.checkall'); ?>
-					</td>
-					<th scope="col">
-						<?php echo Text::_('JGLOBAL_TITLE'); ?>
-					</th>
-					<th scope="col" class="d-none d-md-table-cell">
-						<?php echo Text::_('JFIELD_ALIAS_LABEL'); ?>
-					</th>
-					<th scope="col" class="w-5 d-none d-md-table-cell">
-						<?php echo Text::_('JGRID_HEADING_ID'); ?>
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach ($this->items as $i => $item) { ?>
-					<tr>
-						<td><?php echo HTMLHelper::_('grid.id', $i, $item->id); ?></td>
-						<td>
-							<a href="<?php echo Route::_('index.php?option=com_foobar&task=foo.edit&id=' . $item->id); ?>" title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape($item->title); ?>">
-							<?php echo $this->escape($item->title); ?></a>
-						</td>
-						<td>
-							<?php echo $this->escape($item->alias); ?>
-						</td>
-						<td><?php echo (int) $item->id; ?></td>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <td class="w-1 text-center">
+                        <?php echo HTMLHelper::_('grid.checkall'); ?>
+                    </td>
+                    <th scope="col">
+                        <?php echo Text::_('JGLOBAL_TITLE'); ?>
+                    </th>
+                    <th scope="col" class="d-none d-md-table-cell">
+                        <?php echo Text::_('JFIELD_ALIAS_LABEL'); ?>
+                    </th>
+                    <th scope="col" class="w-5 d-none d-md-table-cell">
+                        <?php echo Text::_('JGRID_HEADING_ID'); ?>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($this->items as $i => $item) { ?>
+                    <tr>
+                        <td><?php echo HTMLHelper::_('grid.id', $i, $item->id); ?></td>
+                        <td>
+                            <a href="<?php echo Route::_('index.php?option=com_foobar&task=foo.edit&id=' . $item->id); ?>" title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape($item->title); ?>">
+                            <?php echo $this->escape($item->title); ?></a>
+                        </td>
+                        <td>
+                            <?php echo $this->escape($item->alias); ?>
+                        </td>
+                        <td><?php echo (int) $item->id; ?></td>
 
-					</tr>
-				<?php } ?>
-			</tbody>
-		</table>
-		<?php endif; ?>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        <?php endif; ?>
 
-		<input type="hidden" name="task" value="">
-		<input type="hidden" name="boxchecked" value="0">
-		<?php echo HTMLHelper::_('form.token'); ?>
-	</div>
+        <input type="hidden" name="task" value="">
+        <input type="hidden" name="boxchecked" value="0">
+        <?php echo HTMLHelper::_('form.token'); ?>
+    </div>
 </form>
-<?php endif; ?>
-
